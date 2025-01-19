3cdac89678f82a617bcf5ce5e06900ccfc44bd54
ljubicicbobo <boban.ljubicic@student.tugraz.at>
2025-01-19 19:23:09 +0100

    Treba popraviti trazeni sector

--- src/mr2_public/tug_turtlebot4/src/NavNode.cpp
+++ src/mr2_public/tug_turtlebot4/src/NavNode.cpp
@@ -42,12 +42,12 @@ NavNode::~NavNode()
 static double goal_x_ = 9999;
 static double goal_y_ = 9999;
 const int num_sectors_ = 72;
-static float threshold_ = 4.25; // prev half-functioning was 8
+static float threshold_ = 7.0; // prev half-functioning was 7.0
 static double a_ = 1.5; // increase to make more sensitive to disntante obstacels
 static double b_ = 0.5; // decress to reduce the impact of distance on obstacle forces
 static double m_max = 0.5;
-static float c_i = 1.0; // incress to account for noisy sensor
-static int m = 8; // increase in clusstered environemnt, decress in open areas
+static float c_i = 1.3; // incress to account for noisy sensor
+static int m = 50; // increase in clusstered environemnt, decress in open areas
 static float angular_scale = 1.0; // tune to make turning more or less aggressive
 static float prev_pose_x = 9999;
 static float prev_pose_y = 9999;
@@ -73,7 +73,7 @@ double NavNode::findBestSector(int target_sector, const std::vector<int>& binary
 
   // Find candidate valley
   for ( int i = 0; i < n; ++i) {
-	RCLCPP_INFO(get_logger(), "%d: %d ",i , binary_histogram[i]);
+	//RCLCPP_INFO(get_logger(), "%d: %d ",i , binary_histogram[i]);
     if (binary_histogram[i] == 0) {
       if (valley_start == -1) {
         //RCLCPP_INFO(get_logger(), " i = %d ", i);
@@ -88,40 +88,55 @@ double NavNode::findBestSector(int target_sector, const std::vector<int>& binary
   if (valley_start != -1) {
     valleys.emplace_back(valley_start, n - 1);
   }
+  double best_sector = -1;
+  double min_cost = std::numeric_limits<double>::max();
 
+  if (valleys.empty()) {
+    RCLCPP_WARN(get_logger(), "No candidate valleys found! Robot is stuck.");
+    return 0.0; // Stop rotation or handle as an edge case.
+  }
+  RCLCPP_INFO(get_logger(), "-------------000-----------");
   for (const auto& valley : valleys) {
     int start = valley.first;
     int end = valley.second;
     int valley_size = ( end - start + 1);
 
-    //RCLCPP_INFO(get_logger(), " Start: %d; End: %d; Valley Size: %d ", start, end, valley_size);
-
-    // Case 1 target in valley
-    if (target_sector >= start && target_sector <= end) {
-      double sector = 2 * M_PI * target_sector / n - M_PI;
-      RCLCPP_INFO(get_logger(), "Case 1: %f", sector);
-      return sector;
+    RCLCPP_INFO(get_logger(), "Start: %d; End: %d; Valley Size: %d ",start, end, valley_size);
+    RCLCPP_INFO(get_logger(), "target_sector: %d;  ", target_sector);
+    //RCLCPP_INFO(get_logger(), "valley size: %d", valley_size);
+    // Case 1 target in valley && valley wide enough
+    if ((target_sector >= start && target_sector <= end) && valley_size > m) {
+      RCLCPP_INFO(get_logger(), "Case 1");
+      return 2 * M_PI * target_sector / n - M_PI;
     }
 
     // Case 2 Valles is wide
-    else if (valley_size > m) {
+    if (valley_size > m) {
+      RCLCPP_INFO(get_logger(), "Case 2");
       int closest = (target_sector < start) ? start : end;
-      double sector = 2 * M_PI * (closest + m / 2) / n - M_PI;
-      RCLCPP_INFO(get_logger(), "Case 2: %f", sector);
-      return sector;
+      double cost = abs(target_sector - closest);
+      if (cost < min_cost) {
+        min_cost = cost;
+        best_sector = 2 * M_PI * (closest + m / 2) / n - M_PI;
+      }
     }
-
     // Case 3
-    else if (valley_size <= m) {
-      //RCLCPP_INFO(get_logger(), "Case 3");
+    if (valley_size <= m) {
+      RCLCPP_INFO(get_logger(), "Case 3");
       int center = (start + end) / 2;
-      double sector = 2 * M_PI * center / n - M_PI;
-      RCLCPP_INFO(get_logger(), "Case 3: %f", sector);
-      return sector;
+      double cost = abs(target_sector - center);
+      if (cost < min_cost) {
+        min_cost = cost;
+        best_sector = 2 * M_PI * center / n - M_PI;
+      }
     }
   }
-
-  return 0;
+  //RCLCPP_INFO(get_logger(), "Best Sector: %f ",best_sector);
+  if (best_sector == -1) {
+    stop_vel = 1;
+    return 0.0;
+  }
+  return best_sector;
 }
 
 void NavNode::publishVelocity(float heading, float d_min, float stop_vel) {
@@ -166,12 +181,21 @@ void NavNode::laserScanCallback(const LaserScan::ConstSharedPtr& scan)
 
   // Init
   std::vector<float> histogram(num_sectors_, 0.0f);
-  double d_max = 0;
+
+
+
   // find farthest active cell to choose a and b a-bdmax=0;
+  double d_max = 0;
   for (size_t i = 0; i < ranges.size(); ++i) {
+    float angle = min_angle + i * angle_increment ;
+
     if (ranges[i] >= scan->range_max || ranges[i] <= scan->range_min) {
       continue;
     }
+    if (i >= 320) {
+      continue;
+    }
+
     if (ranges[i] > d_max) {
       d_max = ranges[i];
     }
@@ -179,22 +203,41 @@ void NavNode::laserScanCallback(const LaserScan::ConstSharedPtr& scan)
 
   b_ = ( m_max / d_max );
   a_ = m_max;
-  //RCLCPP_INFO(get_logger(), "b_ %f, a_ %f", b_, d_max);
+
+  // calculate threshold based on readings
+  int obstacle_count = 0;
+  float total_distance = 0;
+  int valid_readings = 0;
+
+
   // Populate
   for (size_t i = 0; i < ranges.size(); ++i) {
 
     float distance = ranges[i];
     float angle = min_angle + i * angle_increment ;
-    //RCLCPP_INFO(get_logger(), "Angel: %f; i: %ld ", angle, i);
+    //RCLCPP_INFO(get_logger(), "Angle: %f; i: %ld ", angle, i);
     if (distance >= scan->range_max || distance <= scan->range_min) {
       continue;
     }
+
     if (ranges[i] < d_min) {
       d_min = ranges[i];
     }
+    if (i >= 320) {
+       continue;
+    }
+
+    //if (angle >= -M_PI / 8 && angle <= M_PI / 8) {
+      // Angle is in the front (e.g., within ±22.5°)
+    //  continue;
+    //}
+
+    total_distance += ranges[i];
+    valid_readings++;
 
     double force = c_i * c_i * (a_ - b_ * distance);
     force = std::max(0.0, force);
+
     // calculate required sector by normalizing to 0-2pi by adding M_PI
     // calcualting index by 1/(2*M_PI), mapping then to required sec by multiplying
     int sector = static_cast<int>((angle + M_PI) / (2 * M_PI) * num_sectors_) % num_sectors_;
@@ -202,12 +245,32 @@ void NavNode::laserScanCallback(const LaserScan::ConstSharedPtr& scan)
     histogram[sector] += force;
   }
 
+  for (size_t i = 0; i < histogram.size(); ++i) {
+    if (histogram[i] >= threshold_) {
+      obstacle_count += 1;
+    }
+  }
+
+  float avg_distance =(valid_readings > 0) ? total_distance / valid_readings : scan->range_max;
+
+  if (obstacle_count > num_sectors_ * 0.5) {
+    threshold_ = 6.3;
+  } else if (avg_distance > 3) {
+    threshold_ = 7.0;
+  } else {
+    threshold_ = 6.0;
+  }
+
+  RCLCPP_INFO(get_logger(), "Dynamic Threshold: %f, Obstacles: %d, Avg Distance: %f",
+            threshold_, obstacle_count, avg_distance);
+
   std::vector<int> binary_histogram(num_sectors_, 0);
   for (size_t i = 0; i < histogram.size(); ++i) {
     binary_histogram[i] = (histogram[i] >= threshold_) ? 1 : 0;
-    //RCLCPP_INFO(get_logger(), "Angel: %ld; Hist: %f; Binary: %d ", 5*i, histogram[i], binary_histogram[i]);
+    //RCLCPP_INFO(get_logger(), "Angle: %ld; Sector : %ld; Hist: %f; Binary: %d ",5*i, i, histogram[i], binary_histogram[i]);
   }
 
+  //RCLCPP_INFO(get_logger(), "Target: %d", k_target);
   double best_sector = findBestSector(k_target, binary_histogram);
   //RCLCPP_INFO(get_logger(), "Best Sector: %f", best_sector);
   if (1) {
